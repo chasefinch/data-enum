b4c9aacd80c695bb1aa5d7a7ca36ce84942f9a18
Chase Finch <chase@finch.email>
2021-08-27 16:07:14 -0400

    Bump version for 1.1.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='data-enum',
-    version='1.1.0',
+    version='1.1.1',
     author='Chase Finch',
     author_email='chase@finch.email',
     description='An alternative to the built-in Python `enum` implementation',
